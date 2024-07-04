5952aa058395affaf2a088bfc2ab62b8b9bd07c6
Nur Jannah <22024852@myrp.edu.sg>
2024-07-04 15:27:02 +0800

    changes

--- FYP Reviews Plugin/renoku-reviews.php
+++ FYP Reviews Plugin/renoku-reviews.php
@@ -303,7 +303,7 @@ function rp_display_reviews($atts) {
 
 
 
-add_shortcode('company_reviews', 'rp_display_reviews');
+add_shortcode('rp_display_reviews', 'rp_display_reviews');
 
 // Shortcode to display the review submission form (Shernice)
 function rp_review_form_shortcode() {
